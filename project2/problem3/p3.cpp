ced18d37bbb3f43ed1ddb778cc5bf01bbc7423dc
Emilie <emilid2103@gmail.com>
2022-09-22 14:37:35 +0200

    small typo fix

--- project2/problem3/p3.cpp
+++ project2/problem3/p3.cpp
@@ -7,7 +7,7 @@ void testMaxOffdiagSymmetric()
     arma::mat A{{1, 0, 0, 0.5}, {0, 1, -0.7, 0}, {0, -0.7, 1, 0}, {0.5, 0, 0, 1}};
     auto maxElem = maxOffdiagSymmetric(A);
 
-    std::cout << "testing testMaxOffdiagSymmetric with matrix:" << std::endl;
+    std::cout << "testing maxOffdiagSymmetric with matrix:" << std::endl;
     std::cout << A;
     std::cout << "got max=" << maxElem.val << " at position (" << maxElem.i << ", " << maxElem.j << "), correct max is -0.7" << std::endl;
 }
