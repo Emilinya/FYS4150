e65baa07e7407ff285675d99b7663930dbf97348
Emilie <emilid2103@gmail.com>
2022-11-11 23:12:06 +0100

    printing improvements and implemented better critical temp finder

--- project4/problem8/p8.py
+++ project4/problem8/p8.py
@@ -1,79 +1,158 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from dataclasses import dataclass
 
-T_rays = []
-Cv_rays = []
-X_rays = []
 
-def plot_L(L):
-    filename = f"problem8/data-L{L}.dat"
-    with open(filename, "r") as datafile:
-        repeats, nTs = [int(pair.split("=")[1]) for pair in datafile.readline().split(", ")]
-        eps_grid = np.zeros((repeats, nTs))
-        absm_grid = np.zeros_like(eps_grid)
-        Cv_grid = np.zeros_like(eps_grid)
-        X_grid = np.zeros_like(eps_grid)
-        for i in range(repeats):
-            T_ray, eps_ray, absm_ray, Cv_ray, X_ray = np.loadtxt(datafile, skiprows=1, max_rows=nTs).T
-            eps_grid[i, :] = eps_ray
-            absm_grid[i, :] = absm_ray
-            Cv_grid[i, :] = Cv_ray
-            X_grid[i, :] = X_ray
+@dataclass
+class Quantity:
+    mean: np.ndarray
+    std: np.ndarray
 
-    eps_mean = np.mean(eps_grid, axis=0)
-    eps_std = np.std(eps_grid, axis=0)
-    absm_mean = np.mean(absm_grid, axis=0)
-    absm_std = np.std(absm_grid, axis=0)
-    Cv_mean = np.mean(Cv_grid, axis=0)
-    Cv_std = np.std(Cv_grid, axis=0)
-    X_mean = np.mean(X_grid, axis=0)
-    X_std = np.std(X_grid, axis=0)
+    # iter and next are needed to enable use of the explosion operator
+    def __iter__(self):
+        self.i = 0
+        return self
 
-    T_rays.append(T_ray)
-    Cv_rays.append(Cv_mean)
-    X_rays.append(X_mean)
+    def __next__(self):
+        self.i += 1
+        if self.i == 1:
+            return self.mean
+        elif self.i == 2:
+            return self.std
+        else:
+            raise StopIteration
 
-    def plot_w_std(xs, means, stds, label):
-        color = plot_w_std.colors[0]
-        plot_w_std.colors = plot_w_std.colors[1:]
-        plt.fill_between(xs, means - stds, means + stds, color=color, alpha=0.4, edgecolor=color)
-        plt.plot(xs, means, ".-", color=color, label=label)
-    plot_w_std.colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    @classmethod
+    def from_grid(cls, grid: np.ndarray):
+        mean = np.mean(grid, axis=0)
+        std = np.std(grid, axis=0)
 
-    plt.figure(tight_layout=True)
-    plot_w_std(T_ray, eps_mean, eps_std, "$\\left<\\epsilon\\right>$")
-    plot_w_std(T_ray, absm_mean, absm_std, "$\\left<|m|\\right>$")
-    plot_w_std(T_ray, Cv_mean, Cv_std, "$C_V$")
-    # plot_w_std(T_ray, X_mean, X_std, "$\\chi$")
-    plt.ylabel("quantities []")
-    plt.xlabel("T [$J/k_B$]")
-    ymin, _ = plt.ylim()
-    plt.ylim(ymin, 3)
-    plt.legend()
-    plt.savefig(f"imgs/p8_L={L}.svg")
+        return cls(mean, std)
 
-Ls = [40, 60, 80, 100]
-for L in Ls:
-    plot_L(L)
 
-def get_critical_temps(rays, name):
-    critical_temps = []
+@dataclass
+class Data:
+    L: int
+    Ts: np.ndarray
+    eps: Quantity
+    absm: Quantity
+    Cv: Quantity
+    X: Quantity
+
+
+def plot_w_std(xs: np.ndarray, quantity: Quantity, label: str, color, alpha=0.4):
+    plt.fill_between(
+        xs, quantity.mean - quantity.std, quantity.mean + quantity.std,
+        color=color, alpha=alpha, edgecolor=color
+    )
+    plt.plot(xs, quantity.mean, ".-", color=color, label=label)
+
+
+def get_datas(Ls: list[int]) -> list[Data]:
+    datas = []
+    for L in Ls:
+        with open(f"problem8/data-L{L}.dat", "r") as datafile:
+            repeats, nTs = [
+                int(pair.split("=")[1]) for pair in
+                datafile.readline().split(", ")
+            ]
+            eps_grid = np.zeros((repeats, nTs))
+            absm_grid = np.zeros_like(eps_grid)
+            Cv_grid = np.zeros_like(eps_grid)
+            X_grid = np.zeros_like(eps_grid)
+            T_ray = np.zeros(nTs)
+            for i in range(repeats):
+                T_ray, eps_ray, absm_ray, Cv_ray, X_ray = np.loadtxt(
+                    datafile, skiprows=1, max_rows=nTs
+                ).T
+                eps_grid[i, :] = eps_ray
+                absm_grid[i, :] = absm_ray
+                Cv_grid[i, :] = Cv_ray
+                X_grid[i, :] = X_ray
+
+            datas.append(Data(
+                L, T_ray,
+                Quantity.from_grid(eps_grid),
+                Quantity.from_grid(absm_grid),
+                Quantity.from_grid(Cv_grid),
+                Quantity.from_grid(X_grid),
+            ))
+
+    return datas
+
+
+def get_critical_Ts(datas: list[Data], top_ranges):
     plt.figure(tight_layout=True)
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-    for color, T_ray, ray, L in zip(colors, T_rays, rays, Ls):
-        plt.plot(T_ray, ray, label=f"L={L}", color=color)
 
-        com = np.sum(T_ray*ray)/np.sum(ray)
+    critical_temps = []
+    critical_Cvs = []
+    top_Ts_list = []
+    poly_approxs = []
 
-        plt.axvline(com, color=color, linestyle="--")
-        critical_temps.append(com)
-    plt.ylabel(f"{name} []")
+    for data, top_range, color in zip(datas, top_ranges, colors):
+        top_start = np.argmax(data.Ts > top_range[0])
+        top_end = np.argmax(data.Ts > top_range[1])
+
+        top_Ts = data.Ts[top_start:top_end]
+        c, b, a = np.polyfit(top_Ts, data.Cv.mean[top_start:top_end], 2)
+        poly_approx = a + b*top_Ts + c*top_Ts**2
+
+        top_Ts_list.append(top_Ts)
+        poly_approxs.append(poly_approx)
+
+        # analytical maximum of the polynomial approximation
+        critical_T = -b/(2*c)
+        critical_temps.append(critical_T)
+        critical_Cvs.append(a + b*critical_T + c*critical_T**2)
+
+        plot_w_std(data.Ts, data.Cv, label=f"L={data.L}", color=color, alpha=0.1)
+
+    for critical_temp, critical_Cv, color in zip(critical_temps, critical_Cvs, colors):
+        plt.axvline(critical_temp, ymax=(
+            critical_Cv - 0.9)/(2.5 - 0.9), color=color)
+    for top_Ts, poly_approx in zip(top_Ts_list, poly_approxs):
+        plt.plot(top_Ts, poly_approx, "k-")
+
+    plt.xlim(2.15, 2.4)
+    plt.ylim(0.9, 2.5)
+    plt.ylabel(f"$C_V$ []")
     plt.xlabel("T [$J/k_B$]")
     plt.legend()
-    plt.savefig(f"imgs/p8_{name}.svg")
+    plt.savefig(f"imgs/p8_Cv.svg")
 
-    print(critical_temps)
-    print(np.diff(critical_temps))
+    return critical_temps
 
-get_critical_temps(Cv_rays, "Cv")
-get_critical_temps(X_rays, "X")
+
+def plot_Ls(datas: list[Data], critical_Ts):
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    for data, critical_T in zip(datas, critical_Ts):
+        plt.figure(tight_layout=True)
+        plt.axvline(critical_T, color="k",
+                    linestyle="--", label="critical $T$")
+        plot_w_std(data.Ts, data.eps, "$\\left<\\epsilon\\right>$", colors[0])
+        plot_w_std(data.Ts, data.absm, "$\\left<|m|\\right>$", colors[1])
+        plot_w_std(data.Ts, data.Cv, "$C_V$", colors[2])
+
+        plt.ylabel("quantities []")
+        plt.xlabel("T [$J/k_B$]")
+        plt.ylim(-1.88, 3)
+        plt.legend()
+        plt.savefig(f"imgs/p8_L={data.L}.svg")
+
+        plot_w_std(data.Ts, data.X, "$\\chi$", colors[3])
+        plt.legend()
+        plt.ylim(-1.88, 160)
+        plt.savefig(f"imgs/p8_L={data.L}_X.svg")
+
+if __name__ == "__main__":
+    Ls = [40, 60, 80, 100]
+    datas = get_datas(Ls)
+
+    r = 0.02
+    approx_top_Ts = [2.289, 2.283, 2.279, 2.276]
+    top_ranges = [(top-r, top+r) for top in approx_top_Ts]
+    critical_Ts = get_critical_Ts(datas, top_ranges)
+    print(critical_Ts)
+
+    plot_Ls(datas, critical_Ts)
