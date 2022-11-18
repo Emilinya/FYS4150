bf3ccfd7aa397ce54e594a48f70452b7eba3d9ae
Emilie <emilid2103@gmail.com>
2022-11-18 13:47:03 +0100

    now test using actual values

--- project5/src/problem4/data.cpp
+++ project5/src/problem4/data.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-    auto u = get_initial_state(120, {0.5, 0.1, 0}, {0.5, 0.1, 0});
+    auto u = get_initial_state(120, {0.25, 0.05, 200}, {0.5, 0.20, 0});
     u.save("src/problem4/output/u.bin");
 
-    auto V = get_potential(120, 1., {0.5, 0.02, 5, 0.05, 0.1});
+    auto V = get_potential(120, 1., {0.5, 0.02, 2, 0.05, 0.05});
     V.save("src/problem4/output/V.bin");
 
     return 0;
